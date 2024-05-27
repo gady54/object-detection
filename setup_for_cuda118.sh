76f8870c61bbe04681ea2d1570532ceed6f29ad1
gady54 <gadyyy54@gmail.com>
2024-05-27 21:17:09 +0300

    Add files via upload

---
+++ setup_for_cuda118.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Clone the YOLOv5 repository
+git clone https://github.com/ultralytics/yolov5
+cd yolov5 || exit
+
+# Install required Python packages
+pip install -r requirements.txt
+
+# Install additional libraries
+pip install numpy pandas requests flask quart serial mysql  opencv-python mysql-connector-python
+
+# Install torch and cuda 11.8
+
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
